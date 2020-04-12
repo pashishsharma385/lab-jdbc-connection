ffccbdbf6aec5c921fbe464c0bd989c16ae60446
pashishsharma385 <53710102+pashishsharma385@users.noreply.github.com>
2020-04-12 12:22:20 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,6 +1,8 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
@@ -8,6 +10,15 @@ public class Main {
 	public static void main(String args[]) {
 		
 	// Fill your code
+Connection con;
+		con=ConnectionManager.getConnection();
+		if(con!=null)
+			System.out.println(" Connection Established");
+		else
+			System.out.println("Check your connection");
+		
+		
+		
 		
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,49 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-
+import java.io.InputStream;
 
 public class ConnectionManager {
 	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	
+		
+		Properties prop = null ;   		//properties of object
+
+		try {
+			prop = loadPropertiesFile();
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+		}
+		final String driver = prop.getProperty("driver");
+		final String url = prop.getProperty("url");
+		final String username = prop.getProperty("username");
+		final String password = prop.getProperty("password");
+		
+		
+
+		Class.forName(driver); 		// driver class
+		
+		
+
+		Connection con = null;		//creating  connection object
+		con =DriverManager.getConnection(url,username,password);
+		return con;
+	}
+	
+	
+
+	  public static Properties loadPropertiesFile() throws Exception 		  //properties method
+	   {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);in.close();
+		return prop; 
+	   } 
 	
 }
